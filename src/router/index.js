d61b99eb49e4e618372b2d4a3047e1183b80f1a6
Praise <p.o.adebayo218@gmail.com>
2024-02-13 17:48:47 +0100

    initial commit

--- src/router/index.js
+++ src/router/index.js
@@ -10,11 +10,19 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+
+  // Products
+  {
+    path: '/products',
+    name: 'products',
+    component: () => import( /* webpackChunkName: "products" */ '../views/Products.vue'),
+    meta: {
+      title: 'Products'
+    }
+  },
 ]
 
 const router = createRouter({
